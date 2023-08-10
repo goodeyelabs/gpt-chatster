99585cd153161e8eba44d012a9e4673aa1ad0606
Joe Taylor <accounts+github@joetaylor.net>
2023-08-10 08:03:44 +0100

    Added dark mode

--- src/app/theme-switcher.tsx
+++ src/app/theme-switcher.tsx
@@ -14,14 +14,18 @@ export default function ThemeSwitcher() {
 
     if (mounted) {
         return (
-            <div onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className='group transition-all duration-75 items-center cursor-pointer hidden md:grid select-none'>
+            <div onClick={() => setTheme((theme === 'light' || theme === 'system') ? 'dark' : 'light')} className='group transition-all duration-75 items-center cursor-pointer hidden md:grid select-none'>
                 {
                     theme === 'light' &&
                         <MoonIcon className="h-5 w-5 text-zinc-800 dark:text-zinc-300 group-hover:text-blue-400" />
                 }
                 {
-                    theme !== 'light' &&
+                    theme === 'dark' &&
                         <SunIcon className="h-5 w-5 text-zinc-800 dark:text-zinc-300 group-hover:text-blue-400" />
+                }
+                {
+                    theme === 'system' &&
+                        <MoonIcon className="h-5 w-5 text-zinc-800 dark:text-zinc-300 group-hover:text-blue-400" />
                 }
             </div>
         )
