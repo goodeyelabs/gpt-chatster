291e7ba8b0bd2e0865e5f2af0c99a9bf514076e4
Joe Taylor <accounts+github@joetaylor.net>
2023-08-14 08:34:16 +0100

    added dark mode goodeye logo svg

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Image from "next/image"
-import Link from "next/link"
+import { useTheme } from 'next-themes';
 
 function Footer() {
+    const { theme } = useTheme()
+
     return (
         <div className='grid col-span-full md:h-24 px-4 md:px-8 py-8 md:py-0 bg-slate-100 md:bg-white dark:bg-zinc-900 dark:md:bg-zinc-900'>
             <div className='grid h-full w-full mx-auto max-w-7xl items-center justify-items-center md:justify-items-start grid-cols-[1fr] md:grid-cols-[1fr_auto]'>
@@ -12,11 +15,11 @@ function Footer() {
                     <div className='items-center grid grid-flow-col gap-2'>
                         <p className='text-sm text-zinc-500'>Made by</p>
                         <Image 
-                            src='/logo.svg'
+                            src={`/${theme === 'dark' ? 'logo-dark' : 'logo'}.svg`}
                             alt='Goodeye Labs'
                             width='64'
                             height='64'
-                            className='bg-white md:bg-none rounded-full md:rounded-none dark:md:rounded-full p-[4px] md:p-0 w-8 h-8 md:w-6 md:h-6'
+                            className='w-5 h-5 md:w-6 md:h-6'
                         />
                     </div>
                   </div>
