e87ee642a4a693c110882f4671748658e232f8f2
Joe Taylor <accounts+github@joetaylor.net>
2023-08-14 08:16:30 +0100

    added height-aware textarea

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,23 +7,23 @@ import Providers from './providers'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'GPT Chatster | Goodeye Labs',
-  description: 'How GPT chat apps should be.',
+    title: 'GPT Chatster | Goodeye Labs',
+    description: 'How GPT chat apps should be.',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
   return (
       <>
           <GTM />
-            <html lang="en">
-                <body className={inter.className}>
-                    <Providers>{children}</Providers>
-                </body>
-            </html>
+          <html lang="en">
+              <body className={inter.className}>
+                  <Providers>{children}</Providers>
+              </body>
+          </html>
       </>
   )
 }
--- src/components/chat-input.tsx
+++ src/components/chat-input.tsx
@@ -1,10 +1,32 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
-export default function ChatInput() {    
+export default function ChatInput() {   
+    const textareaRef = useRef<HTMLTextAreaElement>(null); 
+    const [prompt, setPrompt] = useState('')
+
+    function handlePromptChange(val:string) {
+        setPrompt(val)
+    }
+
+    useEffect(() => {
+        if (textareaRef && textareaRef.current) {
+            textareaRef.current.style.height = '0px';
+            const scrollHeight = textareaRef.current.scrollHeight;
+            textareaRef.current.style.height = scrollHeight + 'px';
+          }
+    },[textareaRef, prompt])
+    
     return (
-        <form className='bg-none'>
-            <input type='text' placeholder='How can I help?' className='font-semibold text-5xl text-left bg-transparent border-none appearance-none outline-none'></input>
+        <form className='grid w-full bg-none'>
+            <textarea 
+                ref={textareaRef}
+                value={prompt} 
+                onChange={event => handlePromptChange(event.currentTarget.value)}
+                rows={1}
+                placeholder='How can I help?' 
+                className='resize-none w-full px-6 py-3 rounded-lg font-normal tracking-tight text-2xl text-left text-zinc-800 dark:text-stone-300 bg-stone-100 dark:bg-zinc-800 border-none appearance-none outline-none'>
+            </textarea>
         </form>
     ) 
 }
