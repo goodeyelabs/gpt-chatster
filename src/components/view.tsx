15b2c2cc82db54ed29109e318dcd0d8904d46dda
Joe Taylor <accounts+github@joetaylor.net>
2023-08-16 23:10:52 +0100

    extra responsive changes

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -12,7 +12,7 @@ type viewProps = {
 
 export function SideBar() {
     return (
-        <div id='sidebar' className="grid sticky top-0 overflow-y-auto grid-rows-[5rem_1fr_auto] h-[calc(100vh-4.25rem)] w-full border-r border-slate-200 dark:border-neutral-800/80 bg-slate-50/50 dark:bg-redax-light">
+        <div id='sidebar' className="grid sticky top-0 overflow-y-auto grid-rows-[5rem_1fr_auto] h-[calc(100vh-4.25rem)] mh-[-webkit-fill-available] w-full border-r border-slate-200 dark:border-neutral-800/80 bg-slate-50/50 dark:bg-redax-light">
             <Banner />
             <Sessions />
             <SideFooter />
