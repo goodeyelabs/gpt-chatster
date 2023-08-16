3a79da3c85fa372893c41cb5be6490b1ded08763
Joe Taylor <accounts+github@joetaylor.net>
2023-08-16 22:21:23 +0100

    cleared out unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import AppView from '@/components/app-view'
+import View from '@/components/view'
 import { Metadata } from 'next'
 import MessageInput from '@/components/message-input'
 import Sessions from '@/components/sessions'
 import Messages from '@/components/messages'
 import Settings from '@/components/settings'
-import Footer from '@/components/footer'
 
 export const metadata: Metadata = {
     title: 'RedaxGPT - Goodeye Labs',
@@ -12,18 +11,6 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <AppView>
-            {/* <div className='grid grid-cols-[400px_1fr]'>
-                <div className='sticky top-16 grid h-[calc(100vh-4rem)] w-full grid-rows-[1fr_auto] border-r border-slate-200 dark:border-zinc-800 px-8 overflow-y-auto'>
-                    <Sessions />
-                    <Footer />
-                </div>
-                <div className=' grid gap-4 h-full w-full px-20 grid-rows-[1fr_auto] items-start'> */}
-                    {/* <Settings /> */}
-                    {/* <Messages />
-                    <MessageInput /> 
-                </div>
-            </div> */}
-        </AppView>
+        <View />
     )
 }
--- src/components/view.tsx
+++ src/components/view.tsx
@@ -1,52 +1,33 @@
+import Sessions from "./sessions"
+import Banner from "./banner"
+import SideFooter from "./side-footer"
+import MainHeader from "./main-header"
+import Messages from "./messages"
+import MessageInput from "./message-input"
 import Header from "./header"
-import Footer from "./footer"
-import { XMarkIcon } from '@heroicons/react/24/outline'
-
-type modalViewProps = {
-    children: any,
-    title?: string,
-    closeOverlay?: any,
-}
-
-export function ModalView({ children, closeOverlay, title }:modalViewProps) {
-    return (
-        <div className='grid grid-rows-[auto_1fr]'>
-            <div className='border-b border-slate-200 h-[4rem] grid col-span-full grid-flow-col items-center grid-cols-[4rem_1fr_4rem]'>
-                <div>
-                    
-                </div>
-                <div className="w-full grid justify-center">
-                    <p className='font-bold text-lg text-zinc-600 tracking-tight'>{title}</p>
-                </div>
-                <div onClick={closeOverlay} className="h-[4rem] w-[4rem] grid place-content-center select-none hover:text-sky-400 cursor-pointer">
-                    <XMarkIcon className="h-6 w-6" />
-                </div>
-            </div>
-            <div className="grid px-2 md:px-6 pb-[4rem] h-full overflow-y-auto">
-                {children}
-            </div>
-        </div>
-    )
-}
 
 type viewProps = {
-    children: any,
-    noFooter?:boolean,
+    children?: any,
 }
 
-function View({ children, noFooter }:viewProps) {
+function AppView({ children }:viewProps) {    
     return (
-        <div className='w-screen h-screen grid grid-cols-1 grid-rows-[4rem_1fr] bg-white dark:bg-zinc-900 overflow-y-auto'>
-            <Header />
-            <div className="grid w-full max-w-10xl mx-auto">
-                {children}
-                {
-                    !noFooter &&
-                        <Footer />  
-                }
+        <div id='view' className='grid grid-cols-[400px_1fr] grid-rows-[4.25rem_1fr]'>
+            <div id='header' className='grid col-span-2'>
+                <Header />
+            </div>
+            <div id='sidebar' className="sticky top-0 overflow-y-auto grid grid-rows-[5rem_1fr_auto] h-[calc(100vh-4.25rem)] w-full border-r border-slate-200 dark:border-neutral-800/80 bg-slate-50/50 dark:bg-redax-light">
+                <Banner />
+                <Sessions />
+                <SideFooter />
+            </div>
+            <div id='main' className='grid w-full h-[calc(100vh-4.25rem)] overflow-y-auto grid-rows-[5rem_1fr_auto] bg-white dark:bg-redax-dark'>
+                <MainHeader />
+                <Messages />
+                <MessageInput />
             </div>
         </div>
     )
 }
 
-export default View
+export default AppView
