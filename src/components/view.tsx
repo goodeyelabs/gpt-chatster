c09e7437af3bdfa83d654be445852eec395937f5
Joe Taylor <accounts+github@joetaylor.net>
2023-08-16 23:40:01 +0100

    extra responsive changes

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -12,7 +12,7 @@ type viewProps = {
 
 export function SideBar() {
     return (
-        <div id='sidebar' className="grid sticky top-0 overflow-y-auto grid-rows-[5rem_1fr_auto] h-[calc(100vh-4.25rem)] w-full border-r border-slate-200 dark:border-neutral-800/80 bg-slate-50/50 dark:bg-redax-light">
+        <div id='sidebar' className="grid sticky top-0 overflow-y-auto grid-rows-[5rem_1fr_auto] h-full w-full border-r border-slate-200 dark:border-neutral-800/80 bg-slate-50/50 dark:bg-redax-light">
             <Banner />
             <Sessions />
             <SideFooter />
@@ -22,12 +22,12 @@ export function SideBar() {
 
 function AppView({ children }:viewProps) {    
     return (
-        <div id='view' className='grid grid-cols-[1fr] md:grid-cols-[400px_1fr] grid-rows-[4.25rem_1fr] h-full h-screen-ios overflow-y-hidden'>
+        <div id='view' className='grid grid-cols-[1fr] md:grid-cols-[400px_1fr] grid-rows-[4.25rem_1fr] h-screen h-screen-ios overflow-y-hidden'>
             <div id='header' className='grid col-span-2'>
                 <Header />
             </div>
             <SideBar />
-            <div id='main' className='grid w-full h-[calc(100vh-4.25rem)] overflow-y-auto grid-rows-[5rem_1fr_auto] bg-white dark:bg-redax-dark'>
+            <div id='main' className='grid w-full h-full overflow-y-auto grid-rows-[5rem_1fr_auto] bg-white dark:bg-redax-dark'>
                 <MainHeader />
                 <Messages />
                 <MessageInput />
