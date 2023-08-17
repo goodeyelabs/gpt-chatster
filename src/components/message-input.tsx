2d75acdbe7948b3a8437dacdb69c2b574c199979
Joe Taylor <accounts+github@joetaylor.net>
2023-08-17 14:40:37 +0100

    dev

--- src/components/message-input.tsx
+++ src/components/message-input.tsx
@@ -4,7 +4,7 @@ import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { addMessage, setCurrentPrompt } from '@/redux/sessionsReducer'
 import { setScrollMain, setGptResponseIndex } from '@/redux/commonReducer'
 import Button from './button'
-import { Cog6ToothIcon } from '@heroicons/react/24/outline'
+import { BeakerIcon, Cog6ToothIcon } from '@heroicons/react/24/outline'
 
 export default function MessageInput() {   
     const textareaRef = useRef<HTMLTextAreaElement>(null); 
@@ -105,8 +105,9 @@ export default function MessageInput() {
       }, [gptResponse]);
 
     return (
-        <div className='grid gap-3 grid-cols-[auto_1fr_auto] min-h-[3.75rem] px-5 py-4 bg-white border-t border-neutral-200 items-end'>
-        <Button icon={<Cog6ToothIcon />} text='Settings' />    
+        <div className='grid gap-3 grid-cols-[auto_auto_1fr] min-h-[3.75rem] px-5 py-4 bg-white border-t border-neutral-200 items-end'>
+            <Button icon={<Cog6ToothIcon />} text='Settings' />    
+            <Button icon={<BeakerIcon />} text='GPT 3.5' />    
             <textarea 
                 ref={textareaRef}
                 autoFocus
@@ -116,7 +117,6 @@ export default function MessageInput() {
                 placeholder='Send a message' 
                 className='grid w-full min-h-[2.5rem] py-2 px-4 bg-neutral-100 rounded-[1.25rem] resize-none text-base font-medium text-neutral-700 placeholder:text-neutral-500 tracking-tight appearance-none outline-none overflow-y-hidden'>
             </textarea>
-            <Button icon={<Cog6ToothIcon />} text='Settings' />
         </div>
     )
 
