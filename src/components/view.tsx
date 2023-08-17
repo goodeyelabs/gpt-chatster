93e267f87dc628090bb14031633db2802e54fd8b
Joe Taylor <accounts+github@joetaylor.net>
2023-08-17 07:50:59 +0100

    dev

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -13,9 +13,9 @@ type viewProps = {
 export function SideBar() {
     return (
         <div id='sidebar' className="grid sticky top-0 overflow-y-auto grid-rows-[5rem_1fr_auto] h-full w-full border-r border-slate-200 dark:border-neutral-800/80 bg-slate-50/50 dark:bg-redax-light">
-            <Banner />
+            {/* <Banner />
             <Sessions />
-            <SideFooter />
+            <SideFooter /> */}
         </div>
     )
 }
