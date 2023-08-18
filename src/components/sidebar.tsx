30254c2c0580eb394660d39ddc2e6f827293c0b7
Joe Taylor <accounts+github@joetaylor.net>
2023-08-18 09:58:49 +0100

    dev

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,21 +2,18 @@ import MessageInput from "./message-input"
 import Sessions from "./sessions"
 import NewHeader from "./new-header"
 
-export default function NewSideBar() {
+export default function NewSideBar({ closeOverlay }: { closeOverlay:React.MouseEventHandler}) {
     return (
         <div className="grid grid-rows-[auto_1fr] h-screen h-screen-ios overflow-y-auto">
-            <div className="grid sticky top-0 border-b border-gray-200 bg-white">
-            <NewHeader />
+            <div className="grid sticky top-0 border-b border-gray-200 bg-white h-[4rem] px-5 md:px-8 xl:px-12 2xl:px-16 items-center">
+                <p onClick={closeOverlay}>Close this modal</p>
             </div>
             <div className="grid grid-rows-[1fr_auto]">
                 <div className="grid">
                     <Sessions />
-                    <div style={{height:2000}}>
-                        <p>Other</p>
-                    </div>
                 </div>
-                <div className="grid sticky bottom-0 min-h-[4rem]">
-                    <MessageInput />
+                <div className="grid sticky bottom-0 min-h-[4rem] border-t border-neutral-200 items-center">
+                    <p>How's the font size?</p>
                 </div>
             </div>
         </div>
