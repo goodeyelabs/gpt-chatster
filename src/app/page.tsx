223979dc8cbc3a2e18bfad9f7d0d443a567c1034
Joe Taylor <accounts+github@joetaylor.net>
2023-08-21 22:26:32 +0100

    dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import View from '@/components/view'
 import { Metadata } from 'next'
-import MessageInput from '@/components/message-input'
-import Sessions from '@/components/sessions'
-import Messages from '@/components/messages'
-import Settings from '@/components/settings'
 
 export const metadata: Metadata = {
     title: 'RedaxGPT - Goodeye Labs',
