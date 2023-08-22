0e984ce137635110ad6b46aa853178c93403ed3b
Joe Taylor <accounts+github@joetaylor.net>
2023-08-22 08:36:42 +0100

    dev

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -19,29 +19,29 @@ export function SideBar() {
 export default function View({ children }:viewProps) {    
     return (
         <div className="grid h-screen h-screen-ios grid-cols-[minmax(auto,380px)_1fr]">
-            <div className="grid grid-rows-[4rem_1fr] shadow-[1px_0_0_0] shadow-neutral-200 overflow-y-auto">
-                <div className="grid row-start-1 shadow-[0_1px_0_0] shadow-neutral-200 sticky top-0">
+            <div className="grid grid-rows-[4rem_1fr] shadow-[1px_0_0_0] shadow-neutral-200 h-screen h-screen-ios">
+                <div className="grid row-start-1 sticky top-0">
                     <p>Side header</p>
                 </div>
                 <div className="grid row-start-2">
-                    <div className="grid grid-rows-[1fr_4rem]">
+                    <div className="grid grid-rows-[1fr_4rem] overflow-y-auto h-[calc(100vh-4rem)]">
                         <div className="grid">
                             <div className="grid h-[2000px]">
                                 <p>Side content</p>
                             </div>
                         </div>
-                        <div className="grid shadow-[0_-1px_0_0] shadow-neutral-200 sticky bottom-0">
+                        <div className="grid sticky bottom-0">
                             <p>Side footer</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="grid grid-rows-[4rem_1fr] overflow-y-auto">
+            <div className="grid grid-rows-[4rem_1fr] h-screen h-screen-ios">
                 <div className="grid row-start-1 shadow-[0_1px_0_0] shadow-neutral-200 sticky top-0">
                     <p>Main header</p>
                 </div>
                 <div className="grid row-start-2">
-                    <div className="grid grid-rows-[1fr_4rem]">
+                    <div className="grid grid-rows-[1fr_4rem] overflow-y-auto h-[calc(100vh-4rem)]">
                         <div className="grid">
                             <div className="grid h-[2000px]">
                                 <p>Main content</p>
