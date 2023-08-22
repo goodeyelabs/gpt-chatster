e26dffe979e98adb0edb7b62785e5c2a3fe1bccd
Joe Taylor <accounts+github@joetaylor.net>
2023-08-22 10:17:37 +0100

    dev

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -10,37 +10,65 @@ type viewProps = {
     children?: any,
 }
 
+function Header() {
+    return (
+        <div className="grid shadow-[0_1px_0_0] shadow-neutral-200 h-[3.5rem]">
+
+        </div>
+    )
+}
+
+function SideBar() {
+    return (
+        <div className="grid grid-rows-[3.5rem_1fr] shadow-[1px_0_0_0] shadow-neutral-200">
+            <div className="grid shadow-[0_1px_0_0] shadow-neutral-200 sticky top-[3.5rem]">
+                <p>Side header</p>
+            </div>
+            <div className="grid grid-rows-[1fr_3.5rem] overflow-y-auto h-full">
+                <div className="grid h-[2000px]">
+                    <p>Side content</p>
+                </div>
+                <div className="grid shadow-[0_-1px_0_0] shadow-neutral-200 sticky bottom-0">
+                    <p>Side footer</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Main() {
+    return (
+        <div className="grid grid-rows-[3.5rem_1fr]">
+            <div className="grid shadow-[0_1px_0_0] shadow-neutral-200 sticky top-[3.5rem]">
+                <p>Main header</p>
+            </div>
+            <div className="grid grid-rows-[1fr_3.5rem]">
+                <div className="grid h-[1px]">
+                    <p>Main content</p>
+                </div>
+                <div className="grid shadow-[0_-1px_0_0] shadow-neutral-200">
+                    <p>Main footer</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default function View({ children }:viewProps) {    
     return (
-        <div className="grid grid-rows-[3.5rem_3.5rem_1fr_3.5rem] grid-cols-[minmax(auto,380px)_1fr] h-screen h-screen-ios">
-            <div className="grid row-start-1 col-start-1 col-span-full shadow-[0_1px_0_0] shadow-neutral-200">
+        <div className="grid grid-rows-[3.5rem_1fr] grid-cols-[minmax(auto,380px)_1fr] h-screen h-screen-ios">
+            <div className="grid row-start-1 col-span-full shadow-[0_1px_0_0] shadow-neutral-200">
 
             </div>
-            <div className="grid row-start-2 row-span-3 col-start-1 shadow-[1px_0_0_0] shadow-neutral-200">
-
+            <div className="grid row-start-2 col-start-1 shadow-[1px_0_0_0] shadow-neutral-200 overflow-y-auto">
+                <div className="grid h-[2000px]">
+                    <p>Side</p>
+                </div>
             </div>
-
-            <div className="grid row-start-3 col-start-1 shadow-neutral-200">
-                <p>Side content</p>
-            </div>
-
-            <div className="grid row-start-2 col-start-1 shadow-[0_1px_0_0] shadow-neutral-200">
-
-            </div>
-            <div className="grid row-start-4 col-start-1 shadow-[0_-1px_0_0] shadow-neutral-200">
-
-            </div>
-
-            <div className="grid row-start-2 col-start-2 shadow-[0_1px_0_0] shadow-neutral-200">
-
-            </div>
-
-            <div className="grid row-start-3 col-start-2 shadow-neutral-200">
-                <p>Main content</p>
-            </div>
-
-            <div className="grid row-start-4 col-start-2 shadow-[0_-1px_0_0] shadow-neutral-200">
-
+            <div className="grid row-start-2 col-start-2 overflow-y-auto">
+                <div className="grid h-[2000px]">
+                    <p>Main</p>
+                </div>
             </div>
         </div>
     )
