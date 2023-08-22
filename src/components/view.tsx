ff7d910ea7a4c17e5aba371bfd424dbfa2c3c7ac
Joe Taylor <accounts+github@joetaylor.net>
2023-08-22 08:55:28 +0100

    dev

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -18,11 +18,16 @@ export function SideBar() {
 
 export default function View({ children }:viewProps) {    
     return (
-        <div className="grid grid-rows-[4rem_1fr]">
-            <div className="grid row-start-1 shadow-[0_1px_0_0] shadow-neutral-200 sticky top-0">
+        <div className="grid grid-rows-[4rem_1fr] grid-cols-[minmax(auto,380px)_1fr]">
+            <div className="grid row-start-1 row-span-full col-start-1 shadow-[0_1px_0_0] shadow-neutral-200 sticky top-0 h-screen h-screen-ios">
+                <div className="grid h-[2000px]">
+                    <p>Side content</p>
+                </div>
+            </div>
+            <div className="grid row-start-1 col-start-2 shadow-[0_1px_0_0] shadow-neutral-200 sticky top-0">
                 <p>Main header</p>
             </div>
-            <div className="grid row-start-2">
+            <div className="grid row-start-2 col-start-2">
                 <div className="grid grid-rows-[1fr_4rem]">
                     <div className="grid">
                         <div className="grid h-[2000px]">
