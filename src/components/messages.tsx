4ece6aaef201eac7e29b74217b207c2d0e93609a
Joe Taylor <accounts+github@joetaylor.net>
2023-08-23 08:16:52 +0100

    test 3: remove animation from message bubbles

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -11,6 +11,7 @@ export default function Messages() {
     const [mounted, setMounted] = useState(false)
     const mainRef = useRef<HTMLDivElement>(null); 
     const dispatch = useAppDispatch();
+    const [msgs,setMsgs] = useState([])
 
     const list:any = sessions
     const messages = list[activeSession].messages
@@ -27,15 +28,52 @@ export default function Messages() {
         }
     },[mounted,scrollMain])
 
-    if (mounted && list[activeSession]) {
+    useEffect(() => {
+        setMsgs(list[activeSession].messages)
+    },[list[activeSession].messages])
+
+    if (mounted && msgs.length > 0) {
         return (
             <div className='grid py-0 pb-6 lg:pb-3 items-start content-start w-full px-5 sm:px-8 md:px-12 lg:px-16 xl:px-32 bg-white dark:bg-redax'>
                 {
-                    messages.map((m:object, m_idx:number) => {
+                    msgs.map((m:object, m_idx:number) => {
                         const msg:any = m
 
+                        //  TODO: make this bit DRY
+                        //  GPT response bubble
+                        if (msg.author === 'server') {
+                            return (
+                                <div 
+                                    key={m_idx} 
+                                    className='grid px-2.5 md:px-3 xl:px-4 animate-pop-up-from-bottom'
+                                    >
+                                    <div className='grid gap-2 w-[90%] place-self-start justify-start justify-items-start py-4'>
+                                        <div className='grid bg-slate-200/40 dark:bg-redax-light rounded-[10px] px-2.5 md:px-3 xl:px-4'>
+                                            <p className='text-sm font-base tracking-slight leading-relaxed text-gray-950 dark:text-stone-300 py-2'>
+                                                {msg.message || 'New message'}
+                                            </p>
+                                        </div>
+                                        <p className='text-xs text-neutral-400/80 dark:text-neutral-500/70 tracking-normal'><span className='font-base'>RedaxGPT</span> &middot; {mounted ? formatDate(msg.timestamp.toString()) : 'Loading'}</p>
+                                    </div>
+                                </div>
+                            )
+                        }
+
+                        //  User response bubble
                         return (
-                            <></>
+                            <div 
+                                key={m_idx} 
+                                className='grid px-2.5 md:px-3 xl:px-4 animate-pop-up-from-bottom'
+                            >
+                                <div className='grid gap-2 w-[90%] place-self-end justify-end justify-items-end py-4'>
+                                    <div className='grid bg-gradient-to-r from-blue-500 to-sky-400 rounded-[10px] px-2.5 md:px-3 xl:px-4'>
+                                        <p className='text-sm font-base tracking-slight leading-relaxed text-white py-2'>
+                                            {msg.message || 'New message'}
+                                        </p>
+                                    </div>
+                                    <p className='text-xs text-neutral-400/80 dark:text-neutral-500/70 tracking-normal'><span className='font-base'>Stanley</span> &middot; {mounted ? formatDate(msg.timestamp.toString()) : 'Loading'}</p>
+                                </div>
+                            </div>
                         )
                     })    
                 }
